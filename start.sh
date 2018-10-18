634d41ad90f97710b1cafe77dc9720700b2b1299
betisb <bbaheri@kent.edu>
2018-10-17 21:35:25 -0400

    errors

--- start.sh
+++ start.sh
@@ -1,3 +1,10 @@
 export FLASK_APP=webprog.py
 export FLASK_ENV=development
+export FLASK_DEBUG=1
+#python -m smtpd -n -c DebuggingServer localhost:8025
+#export MAIL_SERVER=smtp.googlemail.com
+#export MAIL_PORT=587
+#export MAIL_USE_TLS=1
+#export MAIL_USERNAME=
+#export MAIL_PASSWORD=
 flask run
